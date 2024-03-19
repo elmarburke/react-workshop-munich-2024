df03d61f6be5c9653a31e74846b85ffba72cddb3
Elmar Burke <hi@elmar.codes>
2024-03-19 09:58:16 +0100

    disclosure

--- src/exercises/01-01-disclosure.tsx
+++ src/exercises/01-01-disclosure.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import { FunctionComponent, ReactNode } from 'react'
 import * as ExcersiseLayout from '../components/excercise-layout'
 
@@ -12,13 +13,32 @@ const Disclosure: FunctionComponent<DisclosureProps> = ({
     summary,
     children,
 }) => {
-    return <>{/* Do your magic here */}</>
+    return (
+        <details
+            className={clsx(className, 'border border-gray-200 rounded-lg')}
+        >
+            <summary>{summary}</summary>
+            <div className="bg-gray-50">{children}</div>
+        </details>
+    )
 }
 
 export default function DisclosurePage() {
     return (
         <ExcersiseLayout.Description title="Disclosure">
             <p>Create a disclosure component with what you just learned.</p>
+
+            <Disclosure
+                summary="Simplify React"
+                className="bg-pink-500 open:bg-green-500"
+            >
+                This is the detailed description of how to simplify react
+                applications!
+            </Disclosure>
+
+            <Disclosure summary="Nicht so eine gute Idee ist">
+                Gruende
+            </Disclosure>
         </ExcersiseLayout.Description>
     )
 }
